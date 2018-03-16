ec5d248022f95eead24733f122b133d115fcfdab
Vinicius Barizon <vinicius.barizon@oi.net.br>
2018-03-15 23:37:14 -0300

    google login

---
+++ src/components/login/Google.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="google-signin-btn" class="g-signin2" data-theme="dark"></div>
+</template>
+
+<script>
+export default {
+  methods: {
+    /* eslint-disable */
+    onSignIn(googleUser) {
+      // Useful data for your client-side scripts:
+      var profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId()); // Don't send this directly to your server!
+      console.log('Full Name: ' + profile.getName());
+      console.log('Given Name: ' + profile.getGivenName());
+      console.log('Family Name: ' + profile.getFamilyName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+
+      // The ID token you need to pass to your backend:
+      var id_token = googleUser.getAuthResponse().id_token;
+      console.log('ID Token: ' + id_token);
+    },
+  },
+  mounted() {
+    gapi.signin2.render('google-signin-btn', { // this is the button "id"
+      onsuccess: this.onSignIn // note, no "()" here
+    })
+  }
+  /* eslint-enable */
+};
+</script>
